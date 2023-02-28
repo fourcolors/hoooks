3c078c0ac26bb98d97e7ed18cc17b388ec610fa2
Sterling Cobb <sterlingcobb@gmail.com>
2023-02-27 19:18:41 -0700

    (broken) - ts hooks don't work in RN
    This is my idea for how to fix the issue

--- ProfileView.tsx
+++ ProfileView.tsx
@@ -1,5 +1,21 @@
 import { Profile } from "@lens-protocol/react-native-lens-ui-kit";
+import { useProfile } from "@lens-protocol/react";
+import { View, Text } from "react-native";
+import { decode } from "base-64";
 
 export default function ProfileView({ route, navigation }) {
-  return <Profile profile={route.params.profile} />;
+  const { data: profile, loading } = useProfile({
+    handle: "web3forbasicbs.lens",
+    decode: (base64) => JSON.parse(decode(base64)),
+  });
+
+  if (loading) {
+    return (
+      <View>
+        <Text>Loading...</Text>
+      </View>
+    );
+  }
+
+  return <Profile profile={profile} />;
 }
